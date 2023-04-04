0287d2a16081b9549f2fbfb3df6f79ed53f31a63
taku-min <tw027444www422277@gmail.com>
2023-04-04 21:00:35 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2023_04_04_102457) do
   end
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "create"
+    t.string "content"
     t.bigint "room_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
